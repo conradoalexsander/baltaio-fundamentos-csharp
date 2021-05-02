b6971b3da8b0e561f8fd07a3733a49a664cbef7a
Conrado Alexsander Soares Gonçalves <conradoalexsander@hotmail.com>
2021-05-02 17:08:19 -0300

    Added Struct and Calculator Project

---
+++ Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
+
---
+++ StructProduct/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace StructProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mouse = new Product(1, "mouse gamer", 299.97, EProductType.Product);
+            // Product mouse = new Product(1, "mouse gamer", 299.97);
+
+            var manutenção = new Product(2, "manutenção", 150.00, EProductType.Service);
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+            Console.WriteLine(mouse.Type);
+
+            Console.WriteLine((int)manutenção.Type);
+        }
+    }
+
+    struct Product
+    {
+        public Product(int id, string name, double price, EProductType type)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public EProductType Type { get; set; }
+
+        public double PriceInDolar(double price, double dolar)
+        {
+            return price * dolar;
+        }
+    }
+
+    enum EProductType
+    {
+        Product = 1,
+        Service = 2
+    }
+}
